c4e34b84157dc7d048c4366096bf57e0165afccb
kylearora <kylearora@gmail.com>
2017-09-16 18:56:03 -0500

    Added button to render users on index.html

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+
 function loadFlipCardUsers() {
 
   fetch('http://localhost:3000/users')
@@ -6,10 +7,24 @@ function loadFlipCardUsers() {
   })
   .then(function(json) {
     console.log(json)
+    let users = json
+    var usersList = document.querySelector("#usersList")
+    for (var i = 0; i < users.length; i++) {
+      let li = document.createElement("li")
+      li.innerHTML = users[i].username
+      usersList.appendChild(li)
+    }
   })
   .catch(function(error) {
     console.log('parsing failed', error)
   })
 }
 
-loadFlipCardUsers()
+
+var userButton = document.querySelector("#userButton")
+
+userButton.addEventListener('click',function(){
+
+  loadFlipCardUsers()
+
+})
