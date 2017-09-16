52fa93e82119f517e4f2106aa54f225ecd57bbd9
kylearora <kylearora@gmail.com>
2017-09-16 18:07:37 -0500

    Added client side and successfully fetched the JSON formatted api of users

---
+++ client/main.js
@@ -0,0 +1,15 @@
+function loadFlipCardUsers() {
+
+  fetch('http://localhost:3000/users')
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(json) {
+    console.log(json)
+  })
+  .catch(function(error) {
+    console.log('parsing failed', error)
+  })
+}
+
+loadFlipCardUsers()
